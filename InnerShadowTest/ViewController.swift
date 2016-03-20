bac584ed3ee5b1ddd2637248f98f16e69e384885
ted <postmaster@oneteque.com>
2016-03-20 17:22:13 +0800

    Initial Commit

---
+++ InnerShadowTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InnerShadowTest
+//
+//  Created by ted gao on 20/3/2016.
+//  Copyright © 2016 ted gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
